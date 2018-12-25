aa36cdbb790ff9a6d0a16c92c54e68545eefa8fb
khensura.loveel Love EL <khensura.loveel@gmail.com>
2018-12-25 05:11:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(target, nested) {
+  
+return document.querySelector('.target','#nested');
+  
+}
+
+
+function increaseRankBy(n) {
+  
+  
+  const lis = document.getElementById('app').querySelectorAll('.ranked-list');
+  
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + `${n}`).toString();  
+
+  
+}}
+
+
+function deepestChild() {
+  
+ return document.getElementById('grand-node').querySelector('div div div div div')
+  
+  
+}
